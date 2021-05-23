ea933ee499a8b365c2c5ef49a63266d4f346aa69
Adrianeczka <adka1591@gmail.com>
2021-05-23 15:09:21 +0200

    all project

---
+++ lib/providers/product.dart
@@ -0,0 +1,42 @@
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
+
+class Product with ChangeNotifier {
+  final String id;
+  final String title;
+  final String description;
+  final double price;
+  final String imageUrl;
+  bool isFavorite;
+
+  Product({
+    @required this.id,
+    @required this.title,
+    @required this.description,
+    @required this.price,
+    @required this.imageUrl,
+    this.isFavorite = false,
+  });
+
+  Future<void> toggleFavoriteStatus() async {
+    final oldStatus = isFavorite;
+
+    isFavorite = !isFavorite;
+    notifyListeners();
+    try {
+      final response = await http.patch(
+          Uri.parse(
+              'https://shopp-app-62611-default-rtdb.europe-west1.firebasedatabase.app/products/$id.json'),
+          body: json.encode({'isFavorite': isFavorite}));
+      if (response.statusCode >= 400) {
+        isFavorite = oldStatus;
+        notifyListeners();
+      }
+    } catch (error) {
+      isFavorite = oldStatus;
+      notifyListeners();
+    }
+  }
+}
